c7d917fcf3675af87db53eab8be96733b275d42c
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2024-02-14 22:20:56 -0800

    rating stars as a react fragment

--- src/components/rating-stars/rating-stars.tsx
+++ src/components/rating-stars/rating-stars.tsx
@@ -4,16 +4,16 @@ type RatingProps = {
   rating: number;
 }
 
-function RatingStars({rating}: RatingProps): JSX.Element[] {
-  const stars = [];
-  for (let i = 0; i < STARS_COUNT; i++) {
-    stars.push(
-      <svg width={17} height={16} aria-hidden="true" key={i} data-testid="star-item">
-        <use xlinkHref={i < rating ? '#icon-full-star' : '#icon-star'}></use>
-      </svg>
-    );
-  }
-  return stars;
+function RatingStars({rating}: RatingProps): JSX.Element {
+  return (
+    <>
+      {Array.from({length: STARS_COUNT}, (_, i) => i).map((item) => (
+        <svg key={item} width={17} height={16} aria-hidden="true" data-testid="star-item">
+          <use xlinkHref={item < rating ? '#icon-full-star' : '#icon-star'}></use>
+        </svg>
+      ))}
+    </>
+  );
 }
 
 export default RatingStars;
