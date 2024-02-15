6962c7d37f8216073dd721e78db2fa1c30de521e
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2024-02-14 22:09:21 -0800

    number in tabIndex

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -63,11 +63,11 @@ function Header(): JSX.Element {
               <input className="form-search__input" type="text" autoComplete="off" placeholder="Поиск по сайту" />
             </label>
             <ul className="form-search__select-list">
-              <li className="form-search__select-item" tabIndex="0">Cannonball Pro MX 8i</li>
-              <li className="form-search__select-item" tabIndex="0">Cannonball Pro MX 7i</li>
-              <li className="form-search__select-item" tabIndex="0">Cannonball Pro MX 6i</li>
-              <li className="form-search__select-item" tabIndex="0">Cannonball Pro MX 5i</li>
-              <li className="form-search__select-item" tabIndex="0">Cannonball Pro MX 4i</li>
+              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 8i</li>
+              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 7i</li>
+              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 6i</li>
+              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 5i</li>
+              <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 4i</li>
             </ul>
           </form>
           <button className="form-search__reset" type="reset">
